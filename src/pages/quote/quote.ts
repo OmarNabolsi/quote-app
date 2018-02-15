13dbb2934eb54cf32f42f9c816d1e38298a12917
Omar Mustafa Nabolsi <onabolsi@dso.ae>
2018-02-15 13:25:56 +0400

    Displaying the quote Page as a Modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -15,11 +9,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor (private viewCtrl: ViewController) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose() {
+    this.viewCtrl.dismiss();
   }
-
 }
