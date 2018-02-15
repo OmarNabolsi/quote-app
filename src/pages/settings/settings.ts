ddeabf397ec88b39f5bb0c7ebeac5dfc601ebe2c
Omar.Nabolsi <omar.nabolsi@gmail.com>
2018-02-15 22:47:16 +0400

    Creating Settings Service

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,12 +1,6 @@
+import { SettingsService } from './../../services/settings';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SettingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, Toggle } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor( private settingsService: SettingsService ) {
+
+  }
+  
+  onToggle(toggle: Toggle) {
+    this.settingsService.setBackground(toggle.checked);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingsPage');
+  checkAltBackground() {
+    return this.settingsService.isAltBackground();
   }
-
 }
